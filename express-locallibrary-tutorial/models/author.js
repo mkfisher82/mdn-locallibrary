a516855d0463391fa50d050025e91c4787f5b9d7
Mark Fisher <mark@yourtradesite.co.nz>
2018-02-08 21:24:05 +1300

    Create new author, genre and book features added

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -6,7 +6,7 @@ var AuthorSchema = new Schema (
     {
         first_name: {type: String, required: true, max: 100},
         family_name: {type: String, required: true, max: 100},
-        date_of_birthBirth: {type: Date},
+        date_of_birth: {type: Date},
         date_of_death: {type: Date}
     }
 );
